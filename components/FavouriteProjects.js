aaca961e519c213a6cf9a5f1224614ad36b83719
bigdog3626 <cdm80383@uga.edu>
2022-04-18 10:37:53 -0400

    Updated

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function FavouriteProjects() {
   return (
-    <div className="bg-[#F1F1F1] -mt-40 dark:bg-meadows-150">
+    <div className="bg-[#dadada] -mt-40 dark:bg-meadows-200">
       <div className="max-w-6xl mx-auto">
         <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center">
             Favourite Projects
           </h1>
           <Link href="/projects">
-            <a className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-meadows-700">
+            <a className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white md:text-[#919191] shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-meadows-700">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
